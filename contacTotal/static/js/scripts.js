5bc41f1681a64656d720990fad27e7837f213cc1
CortinezO98 <153117790+CortinezO98@users.noreply.github.com>
2025-03-01 00:47:41 -0500

    Header dinamico lento

--- contacTotal/static/js/scripts.js
+++ contacTotal/static/js/scripts.js
@@ -1,18 +1,19 @@
 window.onscroll = function() {
-    handleScroll();
-  };
-  
-  function handleScroll() {
-    const header = document.getElementById('fullscreen-header');
-    const compactHeader = document.getElementById('compact-header');
-    
-    if (window.scrollY > 100) { 
-        header.classList.add('hide');
-        compactHeader.classList.add('show');
-    } else {
-        header.classList.remove('hide');
-        compactHeader.classList.remove('show');
-    }
+  const fullscreenHeader = document.getElementById('fullscreen-header');
+  const compactHeader = document.getElementById('compact-header');
+  const threshold = 100;
+
+  if (window.scrollY > threshold) {
+    fullscreenHeader.classList.remove('fast');
+    fullscreenHeader.classList.add('slow', 'hide');
+
+    compactHeader.classList.remove('fast');
+    compactHeader.classList.add('slow', 'show');
+  } else {
+    fullscreenHeader.classList.remove('slow', 'hide');
+    fullscreenHeader.classList.add('fast');
+
+    compactHeader.classList.remove('slow', 'show');
+    compactHeader.classList.add('fast');
   }
-  
-  
+};
