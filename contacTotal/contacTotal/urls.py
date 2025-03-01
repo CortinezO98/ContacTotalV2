a8b5d3e97cdc9f3b4744915db13ddb74c92aa33e
CortinezO98 <153117790+CortinezO98@users.noreply.github.com>
2025-03-01 03:34:30 -0500

    Cambios generales del header y creacion de vista de programas, revista

--- contacTotal/contacTotal/urls.py
+++ contacTotal/contacTotal/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from stationRadio.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView, name="index"),
+    path('revista/', revista, name="revista"),
+    path('programas/', programas, name="programas"),
 ]
--- contacTotal/stationRadio/views.py
+++ contacTotal/stationRadio/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import *
+from .views import *
 
 def IndexView(request):
     return render(request, "index.html")
+
+def revista(request):
+    return render(request, 'revista.html')
+
+def programas(request):
+    return render(request, 'programas.html')
