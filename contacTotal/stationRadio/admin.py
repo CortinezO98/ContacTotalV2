7e57d7d3c4a3e6df607906a5521332b3556874e2
luchi151 <lmcferrira@gmail.com>
2025-04-03 12:58:57 -0500

    Revert "Merge branch 'main2' into devL"
    This reverts commit c438095bb482a8c47ca14dac5593128e9e8ad6e2, reversing
    changes made to 1abcd46c320dfd48f68973e538ba347f7b67ea21.

--- contacTotal/stationRadio/admin.py
+++ contacTotal/stationRadio/admin.py
@@ -1,38 +1,3 @@
 from django.contrib import admin
-from .models import *
 
-@admin.register(EdicionRevista)
-class EdicionRevistaAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'fecha_publicacion', 'url')  
-    search_fields = ('titulo', 'descripcion')  
-    list_filter = ('fecha_publicacion',)
-    
-
-
-
-@admin.register(Programa)
-class ProgramaAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'host', 'duracion', 'url_reproducir')  
-    search_fields = ('titulo', 'host')  
-    list_filter = ('host',)  
-
-
-@admin.register(Programacion)
-class ProgramacionAdmin(admin.ModelAdmin):
-    list_display = ('dia', 'programa', 'hora', 'zona_horaria_usuario', 'hora_mexico', 'hora_argentina', 'hora_eeuu', 'hora_bogota')
-
-    def hora_mexico(self, obj):
-        return obj.get_otra_zona_horaria('America/Mexico_City')
-    hora_mexico.short_description = 'Hora México'
-
-    def hora_argentina(self, obj):
-        return obj.get_otra_zona_horaria('America/Argentina/Buenos_Aires')
-    hora_argentina.short_description = 'Hora Argentina'
-
-    def hora_bogota(self, obj):
-        return obj.get_otra_zona_horaria('America/Bogota')
-    hora_bogota.short_description = 'Hora Bogotá'
-
-    def hora_eeuu(self, obj):
-        return obj.get_otra_zona_horaria('America/New_York')
-    hora_eeuu.short_description = 'Hora EEUU'
+# Register your models here.
--- contacTotal/stationRadio/views.py
+++ contacTotal/stationRadio/views.py
@@ -1,24 +1,15 @@
 from django.shortcuts import render
 from .models import *
 from .views import *
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def IndexView(request):
     return render(request, "index.html")
 
 def revista(request):
-    ediciones = EdicionRevista.objects.all().order_by('-fecha_publicacion') 
-    return render(request, 'revista.html', {'ediciones': ediciones})
-
+    return render(request, 'revista.html')
 
 def programas(request):
-    programas_list = Programa.objects.all()  
-    paginator = Paginator(programas_list, 10) 
-
-    page_number = request.GET.get('page')  
-    page_obj = paginator.get_page(page_number)  
-
-    return render(request, 'programas.html', {'page_obj': page_obj})
+    return render(request, 'programas.html')
 
 def podcast(request):
     featured_podcast = Podcast.objects.filter(featured=True).first()
@@ -34,32 +25,3 @@ def podcast(request):
         'podcasts': podcasts
     }
     return render(request, 'podcast.html', context)
-
-
-def radio(request):
-    return render(request, 'radio.html')
-
-
-def quienesSomos(request):
-    return render(request, 'quienesSomos.html')
-
-def programacion(request):
-    programacion_list = Programacion.objects.all()
-
-    for item in programacion_list:
-        item.hora_bogota = item.get_otra_zona_horaria('America/Bogota')  
-        item.hora_mexico = item.get_otra_zona_horaria('America/Mexico_City')  
-        item.hora_argentina = item.get_otra_zona_horaria('America/Argentina/Buenos_Aires')  
-        item.hora_eeuu = item.get_otra_zona_horaria('America/New_York') 
-
-    context = {
-        'programacion': programacion_list,
-    }
-    return render(request, 'programacion.html', context)
-
-
-def tienda(request):
-    return render(request, 'tienda.html')
-
-def contacto(request):
-    return render(request, 'contacto.html')
