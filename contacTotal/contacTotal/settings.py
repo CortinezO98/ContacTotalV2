b0afdad8a8bd6f916b5477be0149ab5cd00c8c8d
luchi151 <lmcferrira@gmail.com>
2025-09-21 16:23:53 -0500

    fix_tts_podcatstemplate
    se añade función de parar al tts cuando se haga cambio de pagina, podcast template se actualiza embebido youtube y publicidad, index se cambia la portada de revista dinamica en ediciones anteriores, setting se actualiza a ES

--- contacTotal/contacTotal/settings.py
+++ contacTotal/contacTotal/settings.py
@@ -142,7 +142,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "es-es"
 TIME_ZONE = os.getenv("DJANGO_TIME_ZONE", "America/Phoenix")
 USE_I18N = True
 USE_TZ = True
